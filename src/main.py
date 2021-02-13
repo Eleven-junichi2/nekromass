90abf36756e10c4643ed4bfb66e4cf12bc194c46
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2021-02-13 23:49:13 +0900

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
-import sys
-
 import pygame
 
 
 def main():
-    pass
+    pygame.init()
+    screen = pygame.
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.update()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
