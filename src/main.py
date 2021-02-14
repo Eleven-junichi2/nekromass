a323a967d4e1795b5ba7ba424541586a7f005157
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2021-02-14 09:33:30 +0900

    add screen filling

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import pygame
+import pygame.display
+import pygame.time
+# import pygame.
 
 
 def main():
     pygame.init()
-    pygame.display.set_mode()
+    screen = pygame.display.set_mode((960, 604))
+    pygame.display.set_caption("Nekromass")
     clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill("black")
         pygame.display.update()
         clock.tick(60)
 
