ecafa0ca0b2582dc03d2b44788f957ac22b15c10
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2021-02-14 09:58:57 +0900

    make some classes to ECS model

--- src/main.py
+++ src/main.py
@@ -4,6 +4,28 @@ import pygame.time
 # import pygame.
 
 
+class World:
+    def __init__(self) -> None:
+        self.resources = []
+
+    def insert(self, resource):
+        self.resources.insert(resource)
+
+
+class Component:
+    def __init__(self) -> None:
+        pass
+
+
+class Entity:
+    def __init__(self) -> None:
+        self.id = None
+        self.components = []
+
+    def register(self, component: Component):
+        self.components.insert(Component)
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((960, 604))
